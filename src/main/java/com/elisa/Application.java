ea4ba8f85f8bb96f7584e7ac5d023a987bf323b5
elisa-adacado <echan@adacado.com>
2015-08-18 16:47:14 -0700

    add

--- src/main/java/com/elisa/Application.java
+++ src/main/java/com/elisa/Application.java
@@ -3,6 +3,7 @@ package com.elisa;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.yammer.metrics.core.Counter;
+import com.yammer.metrics.core.Timer;
 /**
  * Created by elisa on 18/08/15.
  */
@@ -10,15 +11,21 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("spring-context.xml");
+        Timer processTimer = (Timer) context.getBean("processTimer");
+
 
 
         NamePrinter elisaNamePrint = (NamePrinter) context.getBean("elisaNamePrint");
         Counter nameCounter = (Counter) context.getBean("nameCounter");
+        processTimer.time();
         elisaNamePrint.printName();
         nameCounter.inc();
         System.out.println(nameCounter.count());
         //te.spellCheck();
         //Writer te = (Writer) context.getBean("writer");
+        Counter anotherCounter = (Counter) context.getBean("nameCounter");
+        processTimer.stop();
+        System.out.println(anotherCounter.count() + "in" + processTimer.time());
 
     }
 }
--- src/main/java/com/elisa/NameCounter.java
+++
@@ -1,8 +0,0 @@
-package com.elisa;
-import com.yammer.metrics.core.Counter;
-
-/**
- * Created by elisa on 18/08/15.
- */
-public class NameCounter {
-}
